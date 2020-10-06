ed63cb32577895f066b299b2c1038b3ca1160795
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2020-10-06 18:38:53 +0530

    app structure ready and api data fetched

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Cards = (data) => {
+  console.log(data);
+  return <div>Cards</div>;
+};
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CountryPicket = () => {
+  return <div>CountryPicket</div>;
+};
+
+export default CountryPicket;
